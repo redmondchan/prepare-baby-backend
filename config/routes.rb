775ab3c51fd16f920ed87c108f23bad35416f1ff
Redmond Chan <redmondchan92@gmail.com>
2019-04-04 12:01:31 -0400

    auth sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[create]
-      post 'login', to: 'users#create'
+      post '/signup', to: 'users#create'
       get '/profile', to: 'users#show'
 
       resources :babies
