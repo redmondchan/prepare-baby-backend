a00574a8d7351106a7704aee4c96005c1cd1ec9e
Redmond Chan <redmondchan92@gmail.com>
2019-07-26 23:24:06 -0400

    removed the welcome email job, it was only used for testing purposes

--- app/jobs/email_sender.rb
+++
@@ -1,8 +0,0 @@
-class EmailSender < ActiveJob::Base
-  @queue = :emails_queue
-
-  def perform(params)
-    UserMailer.welcome_email(params).deliver_later
-  end
-
-end
--- app/jobs/welcome_email_job.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeEmailJob < ApplicationJob
-  @queue = :emails_queue
-
-  def perform(params)
-    UserMailer.welcome_email(params).deliver_later
-  end
-end
